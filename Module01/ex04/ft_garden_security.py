0c1f31c94656139a8e5e1a091c0311527e2cce0c
bernardofg <bernardofantinelli@gmail.com>
2026-01-07 23:25:10 +0000

    docstring correction

---
+++ Module01/ex04/ft_garden_security.py
@@ -0,0 +1,17 @@
+class SecurePlant:
+
+    def __init__(self, name: str, height: int, age: int):
+        self.name = name
+        self._height = height
+        self._age = age
+
+    def set_height(self) -> None:
+
+    def set_age(self) -> None:
+
+
+def ft_garden_security() -> None:
+
+
+if __name__ == "__main__":
+    ft_garden_security()
