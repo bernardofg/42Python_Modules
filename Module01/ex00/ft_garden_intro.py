2fdc914b4c8bf1ba5fba621734b12c81edd0d8cd
bernardofg <bernardofantinelli@gmail.com>
2026-01-08 15:56:43 +0000

    alfredo

---
+++ Module01/ex00/ft_garden_intro.py
@@ -0,0 +1,19 @@
+def ft_garden_intro():
+    """
+    Docstring for ft_garden_intro
+    """
+    plant = "Rose"
+    height = 25
+    age = 30
+
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {plant}Age{age} days")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print("\n=== End of Program ===")
+
+
+if __name__ == "__main__":
+    """
+    """
+    ft_garden_intro()
